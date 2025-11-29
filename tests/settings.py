31690d7355c6d8e29265c7fedc1d78078e1cffec
johan12345 <johan.forstner@gmail.com>
2025-11-30 00:55:06 +0100

    add CI for tests

--- tests/settings.py
+++ tests/settings.py
@@ -1,9 +1,12 @@
+import os
+
 from evmap_backend.settings import *
 
 DATABASES["default"] = {
     "ENGINE": "django.contrib.gis.db.backends.postgis",
-    "NAME": "evmap_test",
-    "USER": "postgres",
-    "PASSWORD": "postgres",
-    "HOST": "127.0.0.1",
+    "NAME": os.environ.get("TEST_DB_NAME", "evmap_test"),
+    "USER": os.environ.get("TEST_DB_USER", "postgres"),
+    "PASSWORD": os.environ.get("TEST_DB_PASSWORD", "postgres"),
+    "HOST": os.environ.get("TEST_DB_HOST", "127.0.0.1"),
+    "PORT": os.environ.get("TEST_DB_PORT", "5432"),
 }
