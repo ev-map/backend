52b8086ca5cf2aae7bee77b503e75d74d67ce622
Johan von Forstner <johan.forstner@gmail.com>
2025-12-25 16:42:39 +0100

    do not allow deletion of API keys

--- src/evmap_backend/apikeys/admin.py
+++ src/evmap_backend/apikeys/admin.py
@@ -7,5 +7,9 @@ from evmap_backend.apikeys.models import ApiKey
 class ApiKeyAdmin(admin.ModelAdmin):
     list_display = ("description", "truncated_key", "created_at", "active")
 
+    def has_delete_permission(self, request, obj=None):
+        # API keys shall not be deleted
+        return False
+
 
 admin.site.register(ApiKey, ApiKeyAdmin)
