f778890e57e66e575aa0dbb9d97e3b2e715c65f9
johan12345 <johan.forstner@gmail.com>
2026-03-11 23:37:26 +0100

    GoingElectric: update sync logic

--- src/evmap_backend/data_sources/goingelectric/source.py
+++ src/evmap_backend/data_sources/goingelectric/source.py
@@ -8,6 +8,7 @@ from evmap_backend.data_sources import DataSource, DataType, UpdateMethod
 from evmap_backend.data_sources.goingelectric.models import (
     GoingElectricChargeLocation,
     GoingElectricChargepoint,
+    GoingElectricNetwork,
 )
 
 API_URL = "https://api.goingelectric.de"
@@ -43,6 +44,12 @@ class GoingElectricDataSource(DataSource):
 
     def pull_data(self):
         startkey = None
+
+        location_ids_to_delete = set(
+            GoingElectricChargeLocation.objects.values_list("id", flat=True)
+        )
+        sites_created = 0
+
         while True:
             response = get_goingelectric_chargers(startkey=startkey)
 
@@ -51,52 +58,91 @@ class GoingElectricDataSource(DataSource):
 
             for data in response["chargelocations"]:
                 with transaction.atomic():
+                    if data["network"]:
+                        network, _ = GoingElectricNetwork.objects.get_or_create(
+                            name=data["network"]
+                        )
+                    else:
+                        network = None
                     location, created = (
                         GoingElectricChargeLocation.objects.update_or_create(
                             id=data["ge_id"],
-                            name=data["name"],
-                            coordinates=Point(
-                                data["coordinates"]["lng"], data["coordinates"]["lat"]
+                            defaults=dict(
+                                name=data["name"],
+                                coordinates=Point(
+                                    data["coordinates"]["lng"],
+                                    data["coordinates"]["lat"],
+                                ),
+                                address_city=(
+                                    data["address"]["city"]
+                                    if data["address"]["city"]
+                                    else ""
+                                ),
+                                address_country=(
+                                    data["address"]["country"]
+                                    if data["address"]["country"]
+                                    else ""
+                                ),
+                                address_postcode=(
+                                    data["address"]["postcode"]
+                                    if data["address"]["postcode"]
+                                    else ""
+                                ),
+                                address_street=(
+                                    data["address"]["street"]
+                                    if data["address"]["street"]
+                                    else ""
+                                ),
+                                network=network,
+                                url=data["url"],
+                                fault_report=data["fault_report"],
+                                verified=data["verified"],
                             ),
-                            address_city=(
-                                data["address"]["city"]
-                                if data["address"]["city"]
-                                else ""
-                            ),
-                            address_country=(
-                                data["address"]["country"]
-                                if data["address"]["country"]
-                                else ""
-                            ),
-                            address_postcode=(
-                                data["address"]["postcode"]
-                                if data["address"]["postcode"]
-                                else ""
-                            ),
-                            address_street=(
-                                data["address"]["street"]
-                                if data["address"]["street"]
-                                else ""
-                            ),
-                            network=data["network"] if data["network"] else "",
-                            url=data["url"],
-                            fault_report=data["fault_report"],
-                            verified=data["verified"],
                         )
                     )
-                    GoingElectricChargepoint.objects.filter(
+
+                    if created:
+                        sites_created += 1
+
+                    chargepoints = GoingElectricChargepoint.objects.filter(
                         chargelocation=location
-                    ).delete()
-                    for chargepoint in data["chargepoints"]:
-                        GoingElectricChargepoint.objects.create(
-                            chargelocation=location,
-                            type=chargepoint["type"],
-                            power=chargepoint["power"],
-                            count=chargepoint["count"],
+                    ).order_by("type", "power", "count")
+                    if chargepoints.count() != len(data["chargepoints"]) or any(
+                        chargepoint.type != cp["type"]
+                        or chargepoint.power != cp["power"]
+                        or chargepoint.count != cp["count"]
+                        for chargepoint, cp in zip(
+                            chargepoints,
+                            sorted(
+                                data["chargepoints"],
+                                key=lambda x: (x["type"], x["power"], x["count"]),
+                            ),
                         )
+                    ):
+                        chargepoints.delete()
+                        for chargepoint in data["chargepoints"]:
+                            GoingElectricChargepoint.objects.create(
+                                chargelocation=location,
+                                type=chargepoint["type"],
+                                power=chargepoint["power"],
+                                count=chargepoint["count"],
+                            )
+
+                    location_ids_to_delete.discard(location.id)
 
             if "startkey" in response:
                 startkey = response["startkey"]
                 print(startkey)
             else:
                 break
+
+        # Delete all remaining sites that weren't in the input
+        if location_ids_to_delete:
+            GoingElectricChargeLocation.objects.filter(
+                id__in=location_ids_to_delete
+            ).delete()
+            sites_deleted = len(location_ids_to_delete)
+        else:
+            sites_deleted = 0
+
+        print(f"{sites_created} sites created, {sites_deleted} sites deleted")
