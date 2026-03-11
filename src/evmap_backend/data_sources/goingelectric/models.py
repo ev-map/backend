33a2e442ac5426e98cc88c7efb15054bb19b370c
johan12345 <johan.forstner@gmail.com>
2026-03-11 23:37:09 +0100

    GoingElectric: add choices for connector types

--- src/evmap_backend/data_sources/goingelectric/models.py
+++ src/evmap_backend/data_sources/goingelectric/models.py
@@ -33,9 +33,30 @@ class GoingElectricChargeLocation(models.Model):
 
 
 class GoingElectricChargepoint(models.Model):
+    class ConnectorTypes(models.TextChoices):
+        CCS_UNKNOWN = "CCS", "CCS"
+        CHADEMO = "CHAdeMO", "CHAdeMO"
+
+        TYPE_1 = "Typ1", "Type 1"
+        TYPE_2 = "Typ2", "Type 2"
+        TYPE_3 = "Typ3", "Type 3"
+
+        TESLA_SUPERCHARGER_CCS = "Tesla Supercharger CCS", "Tesla Supercharger CCS"
+        TESLA_SUPERCHARGER = "Tesla Supercharger", "Tesla Supercharger"
+        TESLA_HPC = "Tesla HPC", "Tesla Roadster HPC"
+
+        SCHUKO = "Schuko", "Schuko"
+        TYP_13 = "Typ13", "Typ 13"
+        TYP_15 = "Typ15", "Typ 15"
+        TYP_23 = "Typ23", "Typ 23"
+        TYP_25 = "Typ25", "Typ 25"
+        CEE_RED = "CEE Rot", "CEE Red"
+        CEE_BLUE = "CEE Blau", "CEE Blue"
+        CEE_PLUS = "CEE+", "CEE+"
+
     chargelocation = models.ForeignKey(
         GoingElectricChargeLocation, on_delete=models.CASCADE
     )
-    type = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, choices=ConnectorTypes)
     power = models.FloatField()
     count = models.IntegerField()
