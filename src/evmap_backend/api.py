9e47379e3b729c58948f37b8865475f569cd7ab2
johan12345 <johan.forstner@gmail.com>
2026-03-17 22:45:26 +0100

    add preliminary realtime API based on GE ID

--- src/evmap_backend/api.py
+++ src/evmap_backend/api.py
@@ -3,15 +3,20 @@ import logging
 from typing import List, Optional, Tuple
 
 from django.contrib.gis.geos import Polygon
-from ninja import ModelSchema, NinjaAPI
+from ninja import ModelSchema, NinjaAPI, Schema
+from ninja.errors import HttpError
 from ninja.orm import register_field
 from ninja.security import django_auth
 
 from evmap_backend.apikeys.ninja import ApiKeyAuth
+from evmap_backend.chargers.fields import format_evseid
 from evmap_backend.chargers.models import ChargingSite
 from evmap_backend.data_sources import UpdateMethod
+from evmap_backend.data_sources.goingelectric.models import GoingElectricChargeLocation
 from evmap_backend.data_sources.models import UpdateState
 from evmap_backend.data_sources.registry import get_data_source
+from evmap_backend.helpers.database import distinct_on
+from evmap_backend.realtime.models import RealtimeStatus
 
 api = NinjaAPI(urls_namespace="evmap")
 
@@ -35,10 +40,63 @@ class ChargingSitesSchema(ModelSchema):
         fields = "__all__"
 
 
+class RealtimeStatusSchema(Schema):
+    evseid: str
+    physical_reference: Optional[str]
+    status: str
+    power: float
+    connector: str
+
+
+class RealtimeStatusesSchema(Schema):
+    statuses: List[RealtimeStatusSchema]
+
+
 @api.get("/sites", response=List[ChargingSitesSchema], auth=[django_auth, ApiKeyAuth()])
 def sites(request, sw_lat: float, sw_lng: float, ne_lat: float, ne_lng: float):
     region = Polygon.from_bbox((sw_lng, sw_lat, ne_lng, ne_lat))
     return ChargingSite.objects.filter(location__coveredby=region)[:1000]
+
+
+@api.get(
+    "/ge_realtime", response=RealtimeStatusesSchema, auth=[django_auth, ApiKeyAuth()]
+)
+def ge_realtime(request, ge_id: int):
+    try:
+        ge_site = GoingElectricChargeLocation.objects.get(id=ge_id)
+    except GoingElectricChargeLocation.DoesNotExist:
+        raise HttpError(404, "GE location not found")
+
+    matched_site = ge_site.matched_site
+    if matched_site is None:
+        raise HttpError(404, "No matched site for this GE location")
+
+    status = RealtimeStatus.objects.filter(
+        chargepoint__in=matched_site.chargepoints.values_list("id")
+    )
+    latest_status = distinct_on(status, ["chargepoint"], "timestamp")
+
+    if latest_status.count() == 0:
+        raise HttpError(404, "No realtime status available for this site")
+
+    statuses = []
+    for cp in matched_site.chargepoints.all():
+        for con in cp.connectors.all():
+            try:
+                status = latest_status.get(chargepoint_id=cp.id).status
+            except RealtimeStatus.DoesNotExist:
+                status = RealtimeStatus.Status.UNKNOWN
+            statuses.append(
+                RealtimeStatusSchema(
+                    evseid=format_evseid(cp.evseid),
+                    power=con.max_power / 1000,
+                    connector=con.connector_type,
+                    physical_reference=cp.physical_reference,
+                    status=status,
+                )
+            )
+
+    return RealtimeStatusesSchema(statuses=statuses)
 
 
 @api.post("/push/{data_source}")
