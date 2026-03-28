1eb815232b5e71fb8388d96866faef802cb8a856
johan12345 <johan.forstner@gmail.com>
2026-03-28 23:53:26 +0100

    speed up RealtimeStatusAdmin

--- src/evmap_backend/realtime/admin.py
+++ src/evmap_backend/realtime/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from django.core.paginator import Paginator
+from django.db import connection
+from django.utils.functional import cached_property
 
 from evmap_backend.realtime.models import RealtimeStatus
+
+
+class LargeTablePaginator(Paginator):
+    """
+    Overrides the count method of QuerySet objects to speed up pagination.
+    https://gist.github.com/noviluni/d86adfa24843c7b8ed10c183a9df2afe
+    """
+
+    @cached_property
+    def count(self):
+        """
+        Returns an estimated number of records, across all pages.
+        """
+        if not self.object_list.query.where:
+            with connection.cursor() as cursor:
+                # Obtain estimated count (only valid with PostgreSQL)
+                cursor.execute(
+                    "SELECT reltuples FROM pg_class WHERE relname = %s",
+                    [self.object_list.query.model._meta.db_table],
+                )
+                estimate = int(cursor.fetchone()[0])
+                return estimate
+        return super().count
 
 
 class RealtimeStatusAdmin(admin.ModelAdmin):
@@ -14,6 +40,8 @@ class RealtimeStatusAdmin(admin.ModelAdmin):
         "data_source",
     ]
     list_filter = ["status", "timestamp", "data_source"]
+    paginator = LargeTablePaginator
+    show_full_result_count = False
 
 
 # Register your models here.
